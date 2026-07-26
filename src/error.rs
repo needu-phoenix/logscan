9e60d7c127bfc30c249f15c2c3115e328745541b
Chinedu Igbodo <igbodochinedu@gmail.com>
2026-07-26 18:58:04 +0100

    Initial Project Structure

---
+++ src/error.rs
@@ -0,0 +1,7 @@
+use std::{io, path::PathBuf};
+
+
+pub enum ScanError {
+    Io {path: PathBuf, source: io::Error},
+    Parse { line_no: usize},
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod cli;
+pub mod error;
+pub mod parser;
+pub mod stats;
+pub mod output;
