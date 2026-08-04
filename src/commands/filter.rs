64e6c0907523dc68b8aefc84bec391f2fbb3f8a2
Chinedu Igbodo <igbodochinedu@gmail.com>
2026-08-04 10:17:23 +0100

    complete filter command with table and json output

--- src/commands/filter.rs
+++ src/commands/filter.rs
@@ -1,5 +1,59 @@
-use std::io::BufRead;
+use crate::{cli::OutputFormat, commands::parse_line};
+use std::io::{self, BufRead};
+use serde::Serialize;
+use regex::Regex;
 
-pub fn run<T: BufRead>(_reader: T, _status: u16) {
-    println!("filter")
+#[derive(Serialize)]
+struct FilterEntry {
+    ip: String,
+    path: String,
+    size: u64
+}
+
+pub fn run<T: BufRead>(reader: T, status: u16, re: &Regex, format: OutputFormat) -> io::Result<()> {
+    match format {
+        OutputFormat::Json => output_json(reader, status, re)?,
+        OutputFormat::Table => output_table(reader, status, re)?
+    }
+    Ok(())
+}
+
+fn output_json<T: BufRead>(mut reader: T, status: u16, re: &Regex) -> io::Result<()> {
+    let mut line = String::new();
+    let mut matches: Vec<FilterEntry> = Vec::new();
+    while reader.read_line(&mut line)? > 0 {
+        if let Some(log_line) = parse_line(&line, re) {
+            if log_line.status == status {
+                matches.push(FilterEntry { 
+                        ip: log_line.ip, 
+                        path: log_line.path, 
+                        size: log_line.size 
+                })
+            }
+        }
+
+        line.clear();
+    }
+
+    let json = serde_json::to_string_pretty(&matches)
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    println!("{}", json);
+    Ok(())
+}
+
+fn output_table<T: BufRead>(mut reader: T, status: u16, re: &Regex) -> io::Result<()> {
+    let mut line = String::new();
+
+    println!("{:<20}{:<30}{:<5}", "IP", "PATH", "SIZE");
+    while reader.read_line(&mut line)? > 0 {
+        if let Some(log_line) = parse_line(&line, re) {
+            if log_line.status == status {
+                println!("{:<20}{:<30}{:<5}", log_line.ip, log_line.path, log_line.size);
+            }
+        }
+
+        line.clear();
+    }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub fn run(cli: cli::Cli) -> Result<(), Box<dyn Error>> {
     let re = Regex::new(r#"^(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<identity>\S+)\s+(?P<user>\S+)\s+\[(?P<datetime>[^\]]*)\]\s+"(?P<method>\w+)\s+(?P<path>\S+)\s+(?P<protocol>[^"]*)"\s+(?P<status>\d{3})\s+(?P<size>\d+|-)"#)?;
 
     match cli.command {
-        cli::Commands::Filter { status } =>  commands::filter::run(reader, status),
+        cli::Commands::Filter { status } =>  commands::filter::run(reader, status, &re, cli.format)?,
         cli::Commands::Summary => commands::summary::run(reader, &re, cli.format)?,
         cli::Commands::Status => commands::status::run(reader, &re, cli.format)?,
         cli::Commands::Top { number } => commands::top::run(reader, &re, number, cli.format)?
